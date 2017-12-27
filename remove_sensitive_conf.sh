395992b39d3aade15c5c95cc2c994a5955072392
Kevin REMY <kevanescence@hotmail.fr>
2017-12-27 16:53:08 +0100

    Prepare named volume location + some conf fixes

--- remove_sensitive_conf.sh
+++ remove_sensitive_conf.sh
@@ -1,1 +1,1 @@
-cat inventory.ini |sed -e 's/=.*/=/' |sed -e 's/\[[a-z]*\(.*\)\]/\[\1\]/' >> inventory.ini.template
+cat inventory.ini |sed -e 's/=.*/=/' |sed -e 's/\[[a-z]*\(.*\)\]/\[\1\]/' > inventory.ini.template
